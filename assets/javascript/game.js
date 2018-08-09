427539b7436d3b8223e1943f30d01bf1a4307c1b
peraltawave <peraltawave@gmail.com>
2018-08-09 15:31:46 -0700

    commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,70 @@
+    
+
+        var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+        'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// Variables for tracking our w l and guesses
+        var wins = 0;
+        var losses = 0;
+        var guesses = 10;
+
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    console.log(computerChoice)
+
+
+
+   // When the user presses a key it fires this stuff below
+   document.onkeypress = function(event) {
+    var userGuess = event.key;
+
+    if(userGuess === computerChoice){
+        
+        // increase wins count
+        wins++;
+
+        // this will reset guesses to 10 and pick a new letter
+        ResetGlobalVariables();
+
+    }else{
+
+        //decrease guess count
+        guesses--;
+    }
+
+    if(guesses === 0){
+
+        //increase loss count 
+        losses++;
+
+        // Reset the guesses only - but doesn't pick a new letter - should I do this?
+        ResetGuesses();
+        
+    }
+        // This resets the gusses only - i nested this into ResetGlobalVariables too just as an experiment
+        function ResetGuesses(){
+            guesses = 10;
+    }
+
+    // this function resets the computerchoice
+    function ResetGlobalVariables(){
+        ResetGuesses();
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(computerChoice)
+    }
+
+
+
+    document.getElementById('lettersGuessed').innerHTML = "Letter Guessed: " + userGuess;
+
+    // i think this is where i can append the guesses so that they all display instead of just one at a time -- but i haven't figured this out
+
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('losses').innerHTML = "Losses: " + losses;
+    document.getElementById('guesses').innerHTML = "Guesses Left: " + guesses;
+
+};
+        
+            
+            
+           
